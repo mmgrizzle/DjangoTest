e4fc2f15c0c2fc12be033fdf6006bd637d46d38b
Mary Grizzle <mmgrizzle@crimson.ua.edu>
2018-10-03 15:18:15 -0500

    Add project files.

---
+++ DjangoTest/DjangoTest/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for DjangoTest.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', DjangoTest.views.home, name='home'),
+    # url(r'^DjangoTest/', include('DjangoTest.DjangoTest.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
